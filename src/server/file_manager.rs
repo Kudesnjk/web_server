0c5a1fcf9c682b716452943cd29894d2221263d4
Kudesnjk <hetchet.govno@yandex.ru>
2021-03-24 10:49:03 +0300

    Добавить обработку /

--- src/server/file_manager.rs
+++ src/server/file_manager.rs
@@ -13,7 +13,8 @@ pub fn new_root_dir(document_root: &str) -> Option<path::PathBuf> {
 }
 
 pub fn get_file(root_path: &path::Path, filename: &str) -> io::Result<fs::File> {
-    let file_abs_path = root_path.join(path::Path::new(&filename[1..filename.len()]));
+    let file_abs_path = root_path.join(
+        path::Path::new(&filename[1..filename.len()]));
     let file = fs::File::open(file_abs_path)?;
     Ok(file)
 }
--- src/server/request.rs
+++ src/server/request.rs
@@ -4,6 +4,8 @@ pub struct Request <'a>  {
     pub path: &'a str,
 }
 
+const DEFAULT_PATH: &'static str = "/index.html";
+
 impl <'a> Request <'a> {
     pub fn new(buf: &[u8]) -> Option<Request> {
         let request_str = match std::str::from_utf8(buf) {
@@ -13,7 +15,11 @@ impl <'a> Request <'a> {
 
         let mut first_line = request_str.split("\r\n").next()?.split(" ");
         let method = first_line.next()?;
-        let path = first_line.next()?;
+        let mut path = first_line.next()?;
+
+        if path == "/" {
+            path = DEFAULT_PATH
+        }
 
         Some(Request {
             method,
